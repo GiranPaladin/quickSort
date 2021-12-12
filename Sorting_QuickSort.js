52479e716711ab75267bb46a946a5c6026eea6fb
Ajuka David <ajuka.zephiniah@gmail.com>
2021-12-12 18:10:47 +0100

    main source file

---
+++ Sorting_QuickSort.js
@@ -0,0 +1,35 @@
+function pivot(arr, start = 0, end = arr.length+1){
+    function swap(array, i, j){
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp
+    }
+
+    var pivot = arr[start]
+    var swapIdx = start;
+
+    for(var i = start +1; i < arr.length; i++){
+        if(pivot > arr[i]){
+            swapIdx++;
+            swap(arr, swapIdx, i)
+        }
+    }
+    swap(arr,start,swapIdx)
+    return swapIdx
+}
+
+//pivot([-3, 100, 4,8,2,1,5,7,6,3])
+
+function quickSort(arr, left = 0,  right = arr.length - 1){
+    if(left < right ){
+    let pivotIndex = pivot(arr, left, right)
+    //left
+    quickSort(arr, left, pivotIndex-1)
+    //right
+    quickSort(arr,pivotIndex + 1, right)
+    }
+    return arr
+}
+
+
+quickSort([4,6,9,1,2,5])
